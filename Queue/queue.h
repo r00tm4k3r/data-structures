22db9402886ad9f751c5161a80098a79ce8e9874
Mike <m-tabola@mail.ru>
2019-10-21 18:11:02 +0300

    queue.h more easy on the eye

--- Queue/queue.h
+++ Queue/queue.h
@@ -9,15 +9,15 @@ typedef struct node
 
 typedef struct queue
 {
-    int queue_size;
+    int    queue_size;
     size_t memSize;
-    node *front;
-    node *rear;
+    node   *front;
+    node   *rear;
 }queue;
 
 void newQueue(queue *q, size_t memSize);
-int enqueue(queue *, const void *);
-void dequeue(queue *, void *);
-void peekQueue(queue *, void *);
-int getQueueLength(queue *);
+int  enqueue(queue *q, const void *);
+void dequeue(queue *q, void *);
+void peekQueue(queue *q, void *);
+int  getQueueLength(queue *q);
 #endif
